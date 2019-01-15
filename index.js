d4b10a7f556035beb8c85e673c8a7060a187473e
Amir Omidi <amir@aaomidi.com>
2019-01-15 17:13:59 -0500

    Change some logic

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-require("dotenv").config();
-const Twitter = require("twitter");
-const Telegram = require("telegraf/telegram");
-const AsyncPolling = require("async-polling");
+require('dotenv').config();
+const Twitter = require('twitter');
+const Telegram = require('telegraf/telegram');
+const AsyncPolling = require('async-polling');
 
 // Create bot and twitter objects
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
+const params = {
+  screen_name: process.env.TWITTER_USER,
+  tweet_mode: 'extended',
+  since_id: undefined,
+  trim_user: true,
+};
+
 async function twitterTextTokenizer(text, entities) {
-  const textParts = text.split(" ");
+  const textParts = text.split(' ');
   const links = textParts.splice(-2);
-  let articleLink = entities.urls.pop();
+  const articleLink = entities.urls.pop();
   return {
-    tweet: textParts.join(" "),
+    tweet: textParts.join(' '),
     articleLink: articleLink.expanded_url,
-    twitterLink: links[1]
+    twitterLink: links[1],
   };
 }
 
@@ -29,37 +36,43 @@ async function constructPost(text, entities) {
   })`;
 }
 
-async function retrievePosts(bot, params) {
-  const tweets = await client.get("statuses/user_timeline", params);
-  if (!tweets[0]) {
-    return;
+async function retrievePosts(bot) {
+  const options = Object.assign({}, params);
+  if (!options.since_id) {
+    options.count = 1;
   }
 
-  let tweet = tweets[0];
-  if (tweet.retweeted_status) {
-    tweet = tweet.retweeted_status;
+  let tweets = await client.get('statuses/user_timeline', options);
+  if (tweets.length === 0) {
+    return false;
   }
-  const message = await constructPost(
-    decodeURI(tweet.full_text),
-    tweet.entities
-  );
-  bot.sendMessage(process.env.CHANNEL_ID, message, {
-    parse_mode: "Markdown"
+
+  tweets = tweets.reverse();
+  tweets.forEach(async (tweet) => {
+    console.log(tweet);
+    params.since_id = tweet.id_str;
+
+    if (tweet.retweeted_status) {
+      tweet = tweet.retweeted_status;
+    }
+
+    const message = await constructPost(
+      decodeURI(tweet.full_text),
+      tweet.entities,
+    );
+
+    bot.sendMessage(process.env.CHANNEL_ID, message, {
+      parse_mode: 'Markdown',
+    });
   });
-  params.since_id = tweet.id_str;
   return true;
 }
 
 async function start() {
   const bot = new Telegram(process.env.BOT_TOKEN);
-  const params = {
-    screen_name: process.env.TWITTER_USER,
-    since_id: undefined,
-    tweet_mode: "extended"
-  };
 
-  AsyncPolling(async end => {
-    await retrievePosts(bot, params);
+  AsyncPolling(async (end) => {
+    await retrievePosts(bot);
     end();
   }, 10e3).run();
 }
